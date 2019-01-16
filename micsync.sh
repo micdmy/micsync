23037841335af8b96babe52f3f79d0b344fe09b9
Michal Dmytruszynski <michal.dmytruszynski@gigaset.com>
2019-01-16 14:49:45 +0100

    Fixes in calling itemSelection

--- micsync.sh
+++ micsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-function getLocationFiles {
-	echo "$(find /data/projects/micsync/*.location -type f)"
+function getCfgFiles {
+	echo "$(find ./*.location -type f)"
 }
 
 function isSubpath {
@@ -15,6 +15,14 @@ function isSubpath {
 	fi
 }
 
+#function isNumeric {
+#	if [[ ("$1" =~ ^[0-9]+$) ]]; then
+#		true
+#	else
+#		false
+#	fi
+#}
+
 #echo $(isSubpath "$1" "$2")
 #if $(isSubpath "$1" "$2") ; then
 #	echo "true"
@@ -22,59 +30,106 @@ function isSubpath {
 #	echo "false"
 #fi
 
-function foundLocation {
+
+function itemSelection {
+	local counter=0
+	local input=-1
+	if [[ "$#" -gt 1 ]] ; then
+		for item in "$@" ; do
+			((counter++))
+			echo "$counter $item"
+		done
+		while [[ ( ("$input" -lt 1) || ("$input" -gt "$#") ) ]]; do
+			read -a inputArray -p "Type from 1 to $# " 
+			input=${inputArray[0]}
+		done
+		selectedItem=${!input}	
+	else
+		selectedItem=$1
+	fi
+}
+
+function findInWorkingAndBackup {
 	local path="$1"
 	local normalised=()
-	local locationFiles=$(getLocationFiles)
+	local cfgFiles=$(getCfgFiles)
 	local foundHere=false
-	local declare -a foundLocationFiles
+	local foundCfgFiles=()
 	FOUND_WORKING=()
 	FOUNT_BACKUP=()	
-#todo declare as arrays
-#todo if found in many location files, ask which to use
 
-	for locationFile in locationFiles ; do
-		source locationFile
-		foundHere=false
+	for cfgFile in $cfgFiles ; do
+		source $cfgFile
+		foundInWorking=false
+		foundInBackup=false
 		for work in "${WORKING[@]}" ; do
 			normalised=$(realpath "$work")	
 			if $(isSubpath "$normalised" "$path") ; then
-				FOUND_WORKING+=($normalised)
-				foundHere=true
+				foundInWorking=true
 			fi
 		done
 		for backup  in "${BACKUP[@]}" ; do
 			normalised=$(realpath "$backup")	
 			if $(isSubpath "$normalised" "$path") ; then
-				FOUND_BACKUP+=($normalised)
-				foundHere=true
+				foundInBackup=true
 			fi
 		done
-		if $foundHere ; then
-			foundLocationFiles+=($locationFile)	
+		if [[ $foundInBackup || $foundInWorking ]]; then
+			foundCfgFiles+=($cfgFile)	
+		fi
+	done
+	
+	if [[ ${#foundCfgFiles[@]} -gt 1 ]]; then
+		echo "Choose configuration file:"
+		itemSelection "${foundCfgFiles[@]}"
+		selectedCfgFile=$selectedItem
+		echo "Chosen: $selectedCfgFile"
+	elif [[ ${#foundCfgFiles[@]} -eq 1 ]]; then
+		selectedCfgFile=${foundCfgFiles[0]}
+	else
+		echo "Path: $path not found in configuration files:"
+		for cfgFile in cfgFiles ; do
+			echo "$cfgFile"
+		done
+	  exit 	
+	fi
+
+	source $selectedCfgFile
+	for work in "${WORKING[@]}" ; do
+		normalised=$(realpath "$work")	
+		if $(isSubpath "$normalised" "$path") ; then
+			FOUND_WORKING+=($normalised)
+		fi
+	done
+	for backup  in "${BACKUP[@]}" ; do
+		normalised=$(realpath "$backup")	
+		if $(isSubpath "$normalised" "$path") ; then
+			FOUND_BACKUP+=($normalised)
 		fi
 	done
 }
 
-function selectItem {
-	local counter=0
-	local input=-1
-	if [[ "$#" -gt 1 ]] ; then
-		for item in "$@" ; do
-			echo "$counter $item"
-			((counter++))
-		done
-		while "$input" -lt 0 -a $ "$input" -lt "$#"
-	else
-		echo hhh
-		echo $1
-		echo hhh2
-	fi
-}
 echo aaaaaaaaaaaa
-echo "$#"
-selectItem "$@"
+echo $1
+givenPath=$(realpath "$1")
+echo $givenPath
+findInWorkingAndBackup "$givenPath"
 echo bbbbbbbbbbbb
+echo "found working $FOUND_WORKING"
+echo "found backup $FOUND_BACKUP"
+#itemSelection "$@"
+#echo "selectedItems $selectedItems"
+#isNumeric "$1"
+#	echo "$1"
+#	pppp=$(isNumeric "$1")
+#	echo utput
+#	echo "$pppp"
+#	if $(isNumeric "$1") ; then
+#		echo tak
+#	else 
+#		echo nie
+#	fi
+echo ccccccccccccccccc
 
 function backup {
 	for path in "$@" ; do
