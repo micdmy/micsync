6cf67597bd776bbf4f590db339d98afd37a01162
Michal Dmytruszynski <michal.dmytruszynski@gigaset.com>
2019-02-07 05:25:23 +0100

    Work on filetring configs and paths

--- micsync.py
+++ micsync.py
@@ -14,6 +14,9 @@ def printError(msg):
 def printIndent(msg):
     print('    ' + str(msg))
 
+def allEqual(elements):
+    return len(set(elements)) in (0, 1)
+
 def parseInputArguments(arguments):
     modes = [Mode('backup', 'm'),
              Mode('work', 'mdD'),
@@ -57,11 +60,10 @@ def isSubpath(basepath, subpath):
     subpath = os.path.abspath(subpath)
     return  basepath == (os.path.commonpath([basepath, subpath]))
     
-def filterConfig(configs, path):
-    for config in configs:
-        config['work'] = [wPath for wPath in config['work'] if isSubpath(wPath, path)]
-        config['backup'] = [bPath for bPath in config['backup'] if isSubpath(bPath, path)]
-    return configs
+def filterConfig(config, path):
+    config['work'] = [wPath for wPath in config['work'] if isSubpath(wPath, path)]
+    config['backup'] = [bPath for bPath in config['backup'] if isSubpath(bPath, path)]
+    return config
 
 def main(argv):
     mode, paths = parseInputArguments(argv)
@@ -71,8 +73,25 @@ def main(argv):
     if not configs:
         return -1
     
-    filtered = filterConfig(configs, paths[0]) 
+    for config in configs:
+        pathsConfig = [filterConfig(config, path) for path in paths]
 
+////////////////////////////////////////////////////////////////
+TODO refactor
+    filtered = [filterConfig(configs, path) for path in paths]
+    print(type(filtered))
+    print(type(filtered[0]))
+
+    print(str(filtered))
+    if any([(not f['work'] and not f['backup']) for f in filtered]):
+        printError('All of paths given should be configured as WORK xor BACKUP.')
+    elif not allEqual(filtered):
+        printError('All given paths should be in the same WORK xor BACKUP.')
+    elif filtered[0]['work'] and filtered[0]['backup']:
+        printError('Given paths cannot be both in WORK and BACKUP.') 
+    else:
+        print('ALL OK')
+/////////////////////////////////////////////////////////////////
     print(str((filtered)))
     print("MODE:" + str(vars(mode)))
     print("PATHS:" + str(paths))
