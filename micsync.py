1c8213ef013787a45a74ce7a528d7d253c0742de
Michal Dmytruszynski <micdmy2@gmail.com>
2019-03-05 21:29:23 +0100

    backup implemented, not tested

--- micsync.py
+++ micsync.py
@@ -2,11 +2,75 @@
 
 import sys, getopt, json, os.path
 
+def userSelect(numElem):
+    while True:
+        userInput = input('[0-' + str(numElem -1) + ']')
+        if userInput.isnumeric() and int(userInput) in (0, numElem - 1):
+            return int(userInput)
+        elif userInput.isalpha() and userInput in 'Qq':
+            return 
+
+def userSelectWork(work):
+    if not work:
+        return
+    if len(work) == 1:
+        return work[0]
+    print('Choose WORK location (Q to cancel and exit):')
+    for i, w in enumerate(work):
+        print('[' + str(i) + '] ' + w)
+    num = userSelect(len(work))
+    if not num:
+        return
+    return work[num] 
+
+def getAccesiblePaths(paths):
+    return [p for p in paths if os.path.exists(p)]
+
+def prependRoot(root, paths):
+    return [os.path.join(root, p) for p in paths] 
+
+def makeRelative(root, paths):
+    return [os.path.relpath(p, root) for p in paths]
+
+def normalize(path):
+    if isinstance(path, list):
+        return [os.path.realpath(os.path.abspath(p)) for p in path]
+    else:
+        return os.path.realpath(os.path.abspath(path))
 
 class Mode:
     def __init__(self, name, options):
         self.name = name
         self.options = options
+
+class BackupMode(Mode):
+    def __init__(self, name, options):
+        super().__init__(name, options)
+    def loadAndCheck(self, applicable):
+        self.applicable = applicable
+        if self.applicable['fBackup']:
+            self.applicable['sWork'] = userSelectWork(self.applicable('work'))
+            self.applicable['pathsOrigin'] = self.applicable['fBackup'][0]
+        else:
+            self.applicable['sWork'] = self.applicable['fWork'][0]
+            self.applicable['pathsOrigin'] = self.applicable['fWork'][0]
+        if not self.applicable['sWork']:
+            return
+        return True
+    def perform(self, paths, rootPath):
+        relPaths = makeRelative(self.applicable['pathsOrigin'], paths)
+        srcs = prependRoot(self.applicable['sWork'], relPaths)
+        relRootPath = makeRelative(self.applicable['pathsOrigin'], rootPath)
+        for backup in getAccesiblePaths(self.applicable['backup']):
+            dst = prependRoot(backup, relRootPath)[0]
+            print('SRCS: ' + str(srcs))
+            print('DST: ' + dst)
+        return True
+    
+modes = [BackupMode('backup', 'm'),
+         Mode('work', 'mdD'),
+         Mode('transfer', 'mdD'),
+         Mode('tree', 'm')]
 
 def printError(msg):
     print('micsync.py: Error: ' + str(msg)) 
@@ -29,10 +93,6 @@ def xor(a, b):
     return (a and not b) or (not a and b)
 
 def parseInputArguments(arguments):
-    modes = [Mode('backup', 'm'),
-             Mode('work', 'mdD'),
-             Mode('transfer', 'mdD'),
-             Mode('tree', 'm')]
     for mode in modes:
         try:
             opts, args = getopt.getopt(arguments[1:], mode.options, [mode.name])
@@ -40,12 +100,18 @@ def parseInputArguments(arguments):
             retMode.options = [x[0][1] for x in opts if (len(x[0]) == 2
                                                      and x[0][0] == '-'
                                                      and  x[0][1] in mode.options)]
-            paths = [os.path.abspath(arg) for arg in args]
+            paths = normalize(args)
+            print('PPAATTHHSS: ' + str(paths))
             for path in paths:
                 if not os.path.exists(path):
                     printError('Invalid path: ' + path)
-                    return None, None
-            return retMode, args
+                    return None, None, None
+            rootPath = os.path.split(paths[0])[0]
+            for path in paths:
+                if rootPath != os.path.split(path)[0]:
+                    printError('Given paths must be in the same location')
+                    return None, None, None
+            return retMode, paths, rootPath
         except getopt.GetoptError:
             pass
     printError('Valid syntax is:')
@@ -54,7 +120,7 @@ def parseInputArguments(arguments):
         for char in mode.options:
             optString += ' [-' + char + ']'
         printIndent(arguments[0] + ' --' + mode.name + optString + ' path...')
-    return None, None
+    return None, None, None
 
 def readConfigurations(configFileName):
     with open(configFileName, 'r') as configFile:
@@ -67,36 +133,65 @@ def readConfigurations(configFileName):
         return configuration['configs']
 
 def isSubpath(basepath, subpath):
-    basepath = os.path.abspath(basepath)
-    subpath = os.path.abspath(subpath)
     return  basepath == (os.path.commonpath([basepath, subpath]))
+
+def verifyConfigurations(configs, configFileName):
+    if not configs:
+        return
+    for i, config in enumerate(configs):
+        if not 'name' in config:
+            printError("Deficient JSON config file: " + configFileName + ":")
+            printIndent('config nr ' + str(i) + ' has no field \"name\".')
+            return
+        elif not 'work' in config:
+            printError("Deficient JSON config file: " + configFileName + ":")
+            printIndent('config \"' + str(i) + '\" has no field \"work\".')
+            return
+        elif not 'backup' in config:
+            printError("Deficient JSON config file: " + configFileName + ":")
+            printIndent('config \"' + str(i) + '\" has no field \"backup\".')
+            return
+        for k, w1 in enumerate(config['work']):
+            for l, w2 in enumerate(config['work']):
+                if k != l and isSubpath(w1, w2):
+                    printError('Bad work paths in config \"' + config['name'] + '\"')
+                    printIndent('Paths in work cannot be its subpaths.')
+                    return
+        for k, b1 in enumerate(config['backup']):
+            for l, b2 in enumerate(config['backup']):
+                if k != l and isSubpath(b1, b2):
+                    printError('Bad backup paths in config \"' + config['name'] + '\"')
+                    printIndent('Paths in backup cannot be its subpaths.')
+                    return
+        for k, b1 in enumerate(config['backup']):
+            for l, w2 in enumerate(config['work']):
+                if k != l and isSubpath(b1, w2):
+                    printError('Bad backup or work paths in config \"' + config['name'] + '\"')
+                    printIndent('Paths in backup and work cannot be its subpaths.')
+                    return
+        config['backup'] = normalize(config['backup'])
+        config['work'] = normalize(config['work'])
+    return configs
     
 def filterConfig(config, path):
     config['fWork'] = [wPath for wPath in config['work'] if isSubpath(wPath, path)]
     config['fBackup'] = [bPath for bPath in config['backup'] if isSubpath(bPath, path)]
     return config
 
-def userSelectConfig(configs,):
+def userSelectConfig(configs):
     if not configs:
         return
     if len(configs) == 1:
         return configs[0]
     print('Many configs applicable, select one (Q to cancel and exit):')
-    configNumber = 0
-    for config in configs:
+    for configNumber, config in enumerate(configs):
         if config['fWork']:
             print('[' + str(configNumber) + '] in WORK of ' + config['name'] + ': ')
         if config['fBackup']:
             print('[' + str(configNumber) + '] in BACKUP of ' + config['name'] + ': ')
         print('   WORK:   ' + str(config['work'])) 
         print('   BACKUP: ' + str(config['backup']))
-        configNumber += 1
-    while True:
-        userInput = input('[0-' + str(len(configs) - 1) + ']')
-        if userInput.isnumeric() and int(userInput) in (0, len(configs) - 1):
-            return configs[int(userInput)]    
-        elif userInput.isalpha() and userInput in 'Qq':
-            return 
+    return userSelect(len(configs))
 
 def filterApplicableConfigs(configs, paths):
     applicableConfigs = []
@@ -118,11 +213,14 @@ def filterApplicableConfigs(configs, paths):
         printError('None of given paths is in WORK or BACKUP')
     return applicableConfigs
 
+
 def main(argv):
-    mode, paths = parseInputArguments(argv)
-    if not mode or not paths :
+    mode, paths, rootPath = parseInputArguments(argv)
+    if not mode or not paths or not rootPath:
         return -1
-    configs = readConfigurations('./.micsync.json')
+    configFileName = './.micsync.json'
+    configs = readConfigurations(configFileName)
+    configs = verifyConfigurations(configs, configFileName)
     if not configs:
         return -1
     applicables = filterApplicableConfigs(configs, paths)
@@ -131,14 +229,17 @@ def main(argv):
     applicable = userSelectConfig(applicables)
     if not applicable:
         return -1
-        
+    if mode.loadAndCheck(applicable):
+        mode.perform(paths, rootPath)
+    else:
+        return -1
+
+    print("APPLICABLE:")
     print(str(applicable))
-        
     print("MODE:" + str(vars(mode)))
     print("PATHS:" + str(paths))
     print("READING JSON:")
     print(readConfigurations('./.micsync.json'))
-    print("TEST")
 
     
 
